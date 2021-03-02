b90146d210f29f5314daf6970197acc5b9d7ea3f
dan murphy <danielmurph8@gmail.com>
2021-03-02 12:29:08 -0500

    add conditional that checks if a folder exists using stat(), if folder exists skip and return 0. If folder does not exist create it and return status code stored in &info

--- quickstart.cpp
+++ quickstart.cpp
@@ -10,12 +10,36 @@ const char* getEnvVar(const char* envVarToGet) {
     return zshVar;
 }
 
+int setupDirStructure (char* nodeDirPath) {
+
+    // store stat() info in a struct
+    struct stat info;
+    // we can use dot-notation to access information about the folder/file
+    // https://linux.die.net/man/2/stat
+    // similar to running stat test-node in command-line
+
+    if ( stat(nodeDirPath, &info) == -1 ) {
+        // this means stat() was not able to contact the folder
+        // since it does not exist, we create it
+        printf("Test Node directory does not exist, creating it now...");
+
+        int dirResponse = mkdir(nodeDirPath, 0777);
+        return dirResponse;
+    }
+    else {
+        //  this means stat() was able to contact the folder
+        // since it already exists, we skip and return.
+        printf("Test Node directory already exists, skipping...");
+        return 0;
+    };
+}
+
 int main () {
     std::cout << "Executing Script..." << std::endl;
     
     char fullNodePath [50];
     sprintf(fullNodePath, "%s/%s", userHomeDir, nodeDirName);
     
-    printf("Node Directory Path is %s", fullNodePath);
-    int dirResponse = mkdir(fullNodePath, 0777);
+    printf("Node Directory Path is %s\n", fullNodePath);
+    setupDirStructure(fullNodePath);
 }
