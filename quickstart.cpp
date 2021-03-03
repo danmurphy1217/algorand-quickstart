5c82a39644407239a43f3c500a47cc3b1a61f3e2
dan murphy <danielmurph8@gmail.com>
2021-03-03 11:40:17 -0500

    add documentation

--- quickstart.cpp
+++ quickstart.cpp
@@ -15,6 +15,14 @@ const char* getEnvVar(const char* envVarToGet) {
 }
 
 int setupDirStructure (char* nodeDirPath) {
+    /*
+    * check if the path exists. If it does, return 0. If it does not,
+    * create the directory, set it to 777 so that everyone has RWX
+    * permissions, and return from the function.
+    * 
+    * @param nodeDirPath -> the path to create (if it does not already exist).
+    * @return 0 (success).
+    */
 
     // store stat() info in a struct
     struct stat info;
@@ -39,6 +47,14 @@ int setupDirStructure (char* nodeDirPath) {
 }
 
 char* setEnvVar(const char* varName, char* value) {
+    /*
+    * set environment variable where `varName`=`value`.
+    * 
+    * @param varName -> the name of the environment variable.
+    * @param value -> the value to assign to `varName`.
+    * 
+    * @return -> `value`, the value assigned to `varName`.
+    */
 
     setenv(varName, value, true);
 
@@ -46,6 +62,14 @@ char* setEnvVar(const char* varName, char* value) {
 }
 
 void downloadFile(const char* urlToDownload, char* downloadLocation){
+    /*
+    * download a file from `urlToDownload` into `downloadLocation`.
+    * 
+    * @param urlToDownload -> must be a valid URL to download from.
+    * @param downloadLocation -> must be a folder that exists.
+    * 
+    * @return none.
+    */
     char* command;
     sprintf(command, "wget %s -P %s", urlToDownload, downloadLocation);
 
@@ -53,21 +77,45 @@ void downloadFile(const char* urlToDownload, char* downloadLocation){
 }
 
 void changeFilePermissions() {
+    /*
+    * change the permissions of a file to everyone can RWX.
+    * 
+    * @param none.
+    * @return none.
+    */
     int result = chmod("/Users/danielmurphy/test-node/update.sh", S_IRWXU);
     printf("PERMISSIONS: %d", result);
 }
 
 int runUpdateScript() {
+    /*
+    * run the update.sh script
+    * 
+    * @param none
+    * @return 0 (success)
+    */
     system("cd /Users/danielmurphy/test-node && yes | ./update.sh -i -c stable -p ~/test-node -d ~/test-node/data -n");
     return 0;
 }
 
 int startNode() {
+    /*
+    * start the Algorand node by running `./goal node start -d data`.
+    * 
+    * @param none.
+    * @return 0 (success).
+    */
     system("cd /Users/danielmurphy/test-node && ./goal node start -d data");
     return 0;
 };
 
 int checkNodeStatus() {
+    /*
+    * check the status of the Algorand node with `./goal node status -d data`.
+    *
+    * @param none.
+    * @return 0 (success).
+    */
     system("cd /Users/danielmurphy/test-node && ./goal node status -d data");
     return 0;
 };
